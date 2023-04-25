3521b8ce9627e25dc8c4e76454a16ca38aeb04e4
ADMIN <sammuelkioko99@gmail.com>
2023-04-25 16:03:22 +0300

    9th commit

--- app/src/main/java/com/example/kcauniversityfinal/LoginActivity.java
+++ app/src/main/java/com/example/kcauniversityfinal/LoginActivity.java
@@ -1,14 +1,31 @@
 package com.example.kcauniversityfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+
 public class LoginActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    FloatingActionButton fb,google,twitter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+        fb = findViewById(R.id.fab_facebook);
+        google =findViewById(R.id.fab_google);
+        twitter =findViewById(R.id.fab_twitter);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Login"));
+        tabLayout.addTab(tabLayout.newTab().setText("Sign Up"));
     }
 }
