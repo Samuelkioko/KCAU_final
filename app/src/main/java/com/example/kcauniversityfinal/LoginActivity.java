6efa0456213b04fb8c6b07c2833148929d1b6803
ADMIN <sammuelkioko99@gmail.com>
2023-04-25 16:27:18 +0300

    10th commit

--- app/src/main/java/com/example/kcauniversityfinal/LoginActivity.java
+++ app/src/main/java/com/example/kcauniversityfinal/LoginActivity.java
@@ -14,6 +14,8 @@ public class LoginActivity extends AppCompatActivity {
     ViewPager viewPager;
     FloatingActionButton fb,google,twitter;
 
+    float v =0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,5 +29,29 @@ public class LoginActivity extends AppCompatActivity {
 
         tabLayout.addTab(tabLayout.newTab().setText("Login"));
         tabLayout.addTab(tabLayout.newTab().setText("Sign Up"));
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final LoginAdapter adapter = new LoginAdapter(getSupportFragmentManager(), this, tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        fb.setTranslationY(300);
+        google.setTranslationY(300);
+        twitter.setTranslationY(300);
+        tabLayout.setTranslationY(300);
+
+        fb.setAlpha(v);
+        google.setAlpha(v);
+        twitter.setAlpha(v);
+        tabLayout.setAlpha(v);
+
+        fb.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
+        google.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(600).start();
+        twitter.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(800).start();
+        tabLayout.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(300).start();
+
+
     }
 }
